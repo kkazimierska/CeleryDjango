3c1461c200368f7f8597a1580bceb705de9665b4
KAMKA DevOps <kamka@orsted.com>
2023-05-18 10:55:23 +0200

    feat: add timestamp field

--- pinger/models.py
+++ pinger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from typing_extensions import TypedDict
+from django.utils import timezone
 
 class PingStatus(models.TextChoices):
     ONLINE = 'Online'
@@ -16,7 +17,6 @@ class PingerParse(TypedDict):
 class PingerManager(models.Manager):
     pass
 
-# Create your models here.
 class Pings(models.Model):
     windfarm_name = models.CharField(default = "HOW01-GEN", max_length=20)
     turbine_name = models.CharField(default = "X01YCA01UH002", max_length=20)
@@ -27,6 +27,7 @@ class Pings(models.Model):
         null=False
     )
     host = models.GenericIPAddressField(default='212.77.98.9', null=False)
+    last_pinged = models.DateTimeField(verbose_name='Last time the IP was pinged', default=timezone.now)
 
     class Meta:
         db_table='pings'
